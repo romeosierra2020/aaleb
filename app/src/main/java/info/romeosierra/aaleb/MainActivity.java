41dc7cf3edee289cde7f50b071bfa637a42a5762
Richard Powell <richard.powell.henshaws@gmail.com>
2024-10-29 15:01:00 +0000

    Setup

---
+++ app/src/main/java/info/romeosierra/aaleb/MainActivity.java
@@ -0,0 +1,63 @@
+package info.romeosierra.aaleb;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    private static final String TAG = "RAGE- MainActivity";
+
+    /**
+     * Hold a reference to our GLSurfaceView
+     */
+    private GLSurfaceView glSurfaceView;
+    private boolean rendererSet = false;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        glSurfaceView = new GLSurfaceView(this);
+
+
+        // Check if the system supports OpenGL ES 2.0.
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        final boolean supportsEs3 = configurationInfo.reqGlEsVersion >= 0x20000;
+
+        if (supportsEs3) {
+//// Request an OpenGL ES 2.0 compatible context.
+            glSurfaceView.setEGLContextClientVersion(2);
+//// Assign our renderer.
+            glSurfaceView.setRenderer(new MyGLRenderer(this));
+            rendererSet = true;
+
+            setContentView(glSurfaceView);
+        } else {
+            Toast.makeText(this, "This device does not support OpenGL ES 3.0.",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        // The activity must call the GL surface view's onResume() on activity onResume().
+        super.onResume();
+        glSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        // The activity must call the GL surface view's onPause() on activity onPause().
+        super.onPause();
+        glSurfaceView.onPause();
+    }
+}
---
+++ app/src/main/java/info/romeosierra/aaleb/MyGLRenderer.java
@@ -0,0 +1,106 @@
+package info.romeosierra.aaleb;
+
+import android.content.Context;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.util.Log;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import info.romeosierra.aaleb.util.LoggerConfig;
+import info.romeosierra.aaleb.util.ShaderHelper;
+import info.romeosierra.aaleb.util.TextResourceReader;
+
+public class MyGLRenderer implements GLSurfaceView.Renderer {
+    private static final int POSITION_COMPONENT_COUNT = 2;
+    private static final int BYTES_PER_FLOAT = 4;
+    private static final String U_COLOR = "u_Color";
+    private static final String A_POSITION = "a_Position";
+    private final FloatBuffer vertexData;
+    float[] tableVertices = {
+            0f, 0f,
+            0f, 14f,
+            9f, 14f,
+            9f, 0f
+    };
+    float[] tableVerticesWithTriangles = {
+// Triangle 1
+            -0.5f, -0.5f,
+            0.5f, 0.5f,
+            -0.5f, 0.5f,
+// Triangle 2
+            -0.5f, -0.5f,
+            0.5f, -0.5f,
+            0.5f, 0.5f,
+// Line 1
+            -0.5f, 0f,
+            0.5f, 0f,
+// Mallets
+            0f, -0.25f,
+            0f, 0.25f
+    };
+    private Context context;
+    private int program;
+    private int uColorLocation;
+    private int aPositionLocation;
+
+    public MyGLRenderer(Context context) {
+        this.context = context;
+        vertexData = ByteBuffer
+                .allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+        vertexData.put(tableVerticesWithTriangles);
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        String vertexShaderSource = TextResourceReader
+                .readTextFileFromResource(context, R.raw.simple_vertex_shader);
+        Log.i("RAGE-", vertexShaderSource);
+        String fragmentShaderSource = TextResourceReader
+                .readTextFileFromResource(context, R.raw.simple_fragment_shader);
+        int vertexShader = ShaderHelper.compileVertexShader(vertexShaderSource);
+        int fragmentShader = ShaderHelper.compileFragmentShader(fragmentShaderSource);
+        program = ShaderHelper.linkProgram(vertexShader, fragmentShader);
+        if (LoggerConfig.ON) {
+            ShaderHelper.validateProgram(program);
+        }
+        GLES20.glUseProgram(program);
+        uColorLocation = GLES20.glGetUniformLocation(program, U_COLOR);
+        aPositionLocation = GLES20.glGetAttribLocation(program, A_POSITION);
+        vertexData.position(0);
+        GLES20.glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GLES20.GL_FLOAT,
+                false, 0, vertexData);
+        GLES20.glEnableVertexAttribArray(aPositionLocation);
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        GLES20.glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 6);
+        GLES20.glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+        GLES20.glDrawArrays(GLES20.GL_LINES, 6, 2);
+        // Draw the first mallet blue.
+        GLES20.glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
+        GLES20.glDrawArrays(GLES20.GL_POINTS, 8, 1);
+// Draw the second mallet red.
+        GLES20.glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+        GLES20.glDrawArrays(GLES20.GL_POINTS, 9, 1);
+
+    }
+
+}
